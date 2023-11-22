cb3ce705cbf8ea38500c577af6343a0a91cf3bf7
Hongming <53244544+skyatmoon@users.noreply.github.com>
2023-11-22 10:17:49 -0600

    Delete src/main directory

--- src/main/java/edu/rice/comp504/model/strategy/IStrategyFac.java
+++
@@ -1,9 +0,0 @@
-package edu.rice.comp504.model.strategy;
-
-public interface IStrategyFac {
-    /**
-     * Create a simple ghost strategy.
-     * @return simple ghost strategy
-     */
-    public IStrategy makeStrategy(String strategy);
-}
